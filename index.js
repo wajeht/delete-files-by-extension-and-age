a8264fc069e1512db56e6a333916ba76cdc127cc
wajeht <58354193+wajeht@users.noreply.github.com>
2021-06-12 19:08:07 -0500

    refactor!: changed folder structor

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const DeleteFileByExtensionAndAge = require('./DeleteFileByExtensionAndAge.js');
+const DeleteFileByExtensionAndAge = require('./src/DeleteFileByExtensionAndAge.js');
 
-const CURRENT_PATH = '/Users/konyein/Desktop/test';
-const FILE_EXTENSION = 'json';
-const DAYS = 14;
-
-// -------------------- MAIN --------------------
+// ------------------------------ MAIN ------------------------------
 (function main() {
+    // constants
+    const CURRENT_PATH = 'Users/konyein/Desktop/test';
+    const FILE_EXTENSION = 'json';
+    const DAYS = 14;
+
+    // call the class we created and do the operation
     const myTask = new DeleteFileByExtensionAndAge();
     myTask.getFilesRecursively(CURRENT_PATH, FILE_EXTENSION, DAYS);
     myTask.printFilesToDelete();
